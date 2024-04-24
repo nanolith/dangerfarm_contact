ff48071a064b3976d72574e37fed928d0f6feaaa
Justin Handville <nanolith@gmail.com>
2024-04-23 22:02:40 -0400

    contact_form_create decl.

--- include/dangerfarm_contact/data/contact_form.h
+++ include/dangerfarm_contact/data/contact_form.h
@@ -14,3 +14,19 @@ struct contact_form
     uint64_t comment_size;
     char data[];
 };
+
+/**
+ * \brief Create a contact form instance given a name, email, and comment.
+ *
+ * \param form          Pointer to the pointer to receive the form on success.
+ * \param name          The name for this contact.
+ * \param email         The email for this contact.
+ * \param comment       The comment for this contact.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int contact_form_create(
+    contact_form** form, const char* name, const char* email,
+    const char* comment);
