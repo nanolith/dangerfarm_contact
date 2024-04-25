7d6329bd8f48d1fc9b519c0a23332e3e900823a0
Justin Handville <nanolith@gmail.com>
2024-04-25 19:25:49 -0400

    string_release impl.

---
+++ src/libcontact/string_release.c
@@ -0,0 +1,23 @@
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/string.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * \brief Given an allocated string, clear and release it.
+ *
+ * \param str           The string to release.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int string_release(char* str)
+{
+    size_t len = strlen(str);
+
+    memset(str, 0, len);
+    free(str);
+
+    return STATUS_SUCCESS;
+}
