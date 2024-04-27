5e91e2640516be136465d0530769c3ba0b3e7fb5
Justin Handville <nanolith@gmail.com>
2024-04-26 22:32:51 -0400

    Ensure that malloc may fail in model checks.

--- models/string_create_release.mk
+++ models/string_create_release.mk
@@ -1,5 +1,6 @@
 ALL:
 	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
 	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
 	--drop-unused-functions \
 	--unwind 10 --unwinding-assertions \
