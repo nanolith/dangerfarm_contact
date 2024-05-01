da37a92a12cf6c33a3a1dffcfef1169220e19405
Justin Handville <nanolith@gmail.com>
2024-04-30 22:05:40 -0400

    Added multibyte read raw contination error.

--- include/dangerfarm_contact/status_codes.h
+++ include/dangerfarm_contact/status_codes.h
@@ -3,3 +3,4 @@
 #define STATUS_SUCCESS                                        0
 #define ERROR_GENERAL_OUT_OF_MEMORY                           1
 #define ERROR_READ_MULTIBYTE_EOF                              2
+#define ERROR_READ_MULTIBYTE_RAW_CONTINUATION                 3
