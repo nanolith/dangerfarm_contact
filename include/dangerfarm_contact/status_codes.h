2db44e25b1a7af8249c1cd01bfde81207adce1e0
Justin Handville <nanolith@gmail.com>
2024-04-30 22:04:48 -0400

    Added multibyte read eof error to status codes.

--- include/dangerfarm_contact/status_codes.h
+++ include/dangerfarm_contact/status_codes.h
@@ -1,4 +1,5 @@
 #pragma once
 
-#define STATUS_SUCCESS  0
-#define ERROR_GENERAL_OUT_OF_MEMORY  1
+#define STATUS_SUCCESS                                        0
+#define ERROR_GENERAL_OUT_OF_MEMORY                           1
+#define ERROR_READ_MULTIBYTE_EOF                              2
