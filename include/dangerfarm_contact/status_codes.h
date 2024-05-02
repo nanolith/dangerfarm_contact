7cc50046cfbabd14662853d82c7890674d591169
Justin Handville <nanolith@gmail.com>
2024-05-01 21:24:35 -0400

    Added multibyte read invalid cont. error.

--- include/dangerfarm_contact/status_codes.h
+++ include/dangerfarm_contact/status_codes.h
@@ -4,3 +4,4 @@
 #define ERROR_GENERAL_OUT_OF_MEMORY                           1
 #define ERROR_READ_MULTIBYTE_EOF                              2
 #define ERROR_READ_MULTIBYTE_RAW_CONTINUATION                 3
+#define ERROR_READ_MULTIBYTE_INVALID_CONTINUATION             4
