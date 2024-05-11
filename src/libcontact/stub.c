268ace07ba669f73d32f5583aaa8aa5ae2c8d72a
Justin Handville <nanolith@gmail.com>
2024-05-10 22:37:51 -0400

    Added util socket header to stub.

--- src/libcontact/stub.c
+++ src/libcontact/stub.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
 #include <dangerfarm_contact/data/contact_form.h>
 #include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
 #include <dangerfarm_contact/util/string.h>
