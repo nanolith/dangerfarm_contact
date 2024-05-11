8695c2429d569a064c8eb61aac8c376ffdbfc2cc
Justin Handville <nanolith@gmail.com>
2024-05-10 22:32:26 -0400

    contact_form_extract_subject impl.

---
+++ src/libcontact/contact_form_extract_subject.c
@@ -0,0 +1,23 @@
+#include <dangerfarm_contact/data/contact_form.h>
+#include <dangerfarm_contact/util/string.h>
+
+/**
+ * \brief Given a \ref contact_form, extract the subject as a string.
+ *
+ * \note This string is owned by the caller and must be reclaimed using \ref
+ * string_release.
+ *
+ * \param subject       Pointer to the char pointer to receive the subject on
+ *                      success.
+ * \param form          The \ref contact_form instance for this operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int contact_form_extract_subject(char** subject, const contact_form* form)
+{
+    size_t offset = form->name_size + form->email_size;
+
+    return string_create(subject, form->data + offset, form->subject_size);
+}
