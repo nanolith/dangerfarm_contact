2c641345dd435cd6f53c8e02a83ef5fcbf3c4ab5
Justin Handville <nanolith@gmail.com>
2024-05-11 21:31:51 -0400

    socket_read_contact_form_header impl.

---
+++ src/libcontact/socket_read_contact_form_header.c
@@ -0,0 +1,52 @@
+#include <dangerfarm_contact/data/contact_form.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
+
+/**
+ * \brief Read a contact form header from a socket.
+ *
+ * \note This reads the size data for the contact form, into a stack allocated
+ * contact form header. The resulting form should NOT be released.
+ *
+ * \param hdr           Pointer to the \ref contact_form header to read.
+ * \param s             The socket from which the value is read.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int socket_read_contact_form_header(contact_form* hdr, int s)
+{
+    int retval;
+
+    /* read the name size. */
+    retval = socket_read_uint64(&hdr->name_size, s);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* read the email size. */
+    retval = socket_read_uint64(&hdr->email_size, s);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* read the subject size. */
+    retval = socket_read_uint64(&hdr->subject_size, s);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* read the comment size. */
+    retval = socket_read_uint64(&hdr->comment_size, s);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* success. */
+    return STATUS_SUCCESS;
+}
