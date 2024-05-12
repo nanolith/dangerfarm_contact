e3e13230a7a66c9e74bd58ebad61540176f8c4a2
Justin Handville <nanolith@gmail.com>
2024-05-11 21:31:06 -0400

    socket_read_uint64 impl.

---
+++ src/libcontact/socket_read_uint64.c
@@ -0,0 +1,27 @@
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
+#include <unistd.h>
+
+/**
+ * \brief Read a uint64_t value from a socket.
+ *
+ * \param val           Pointer to the value to read.
+ * \param s             The socket from which the value is read.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int socket_read_uint64(uint64_t* val, int s)
+{
+    ssize_t read_bytes = 0;
+
+    /* read the value. */
+    read_bytes = read(s, val, sizeof(*val));
+    if (read_bytes < 0 || (size_t)read_bytes != sizeof(*val))
+    {
+        return ERROR_SOCKET_READ;
+    }
+
+    return STATUS_SUCCESS;
+}
