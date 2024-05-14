b66f7cb94233cc7925c96770500ba2004830dc35
Justin Handville <nanolith@gmail.com>
2024-05-13 22:41:10 -0400

    Fixed access checks.

--- models/shadow/unix/write.c
+++ models/shadow/unix/write.c
@@ -18,10 +18,10 @@ ssize_t write(int fd, const void* buf, size_t nbytes)
     /* read a byte from the beginning and end of the buffer to check access. */
     if (nbytes > 0)
     {
-        char byte0 = buf[0];
-        MODEL_ASSERT(byte0 == buf[0]);
-        char byte1 = buf[nbytes - 1];
-        MODEL_ASSERT(byte1 == buf[nbytes - 1]);
+        char byte0 = cbuf[0];
+        MODEL_ASSERT(byte0 == cbuf[0]);
+        char byte1 = cbuf[nbytes - 1];
+        MODEL_ASSERT(byte1 == cbuf[nbytes - 1]);
     }
 
     /* does this write succeed? */
