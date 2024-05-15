35c9ba9196c0dbffa0d6cd7295c78981e68cbb16
Justin Handville <nanolith@gmail.com>
2024-05-14 23:50:06 -0400

    Model check of string_create_nondet shadow method.

---
+++ models/string_create_nondet.mk
@@ -0,0 +1,9 @@
+ALL:
+	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
+	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
+	--drop-unused-functions \
+	--unwind 10 --unwinding-assertions \
+	-I ../include \
+	shadow/libcontact/string/string_create_nondet.c \
+	string_create_nondet_main.c
---
+++ models/string_create_nondet_main.c
@@ -0,0 +1,23 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/string.h>
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    char* str;
+    const char* INPUT = "1234567890abcdefg";
+    size_t INPUT_SIZE = 18;
+
+    retval = string_create(&str, INPUT, INPUT_SIZE);
+    if (STATUS_SUCCESS != retval)
+    {
+        /* the only failure case this implementation supports is an allocation
+         * error. */
+        MODEL_ASSERT(ERROR_GENERAL_OUT_OF_MEMORY == retval);
+        return 1;
+    }
+
+    /* clean up. */
+    free(str);
+}
