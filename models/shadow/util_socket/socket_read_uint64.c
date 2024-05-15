782660cc0685ff86702caf9e41ebd78499ccf49b
Justin Handville <nanolith@gmail.com>
2024-05-14 23:41:12 -0400

    socket_read_uint64 shadow impl.

---
+++ models/shadow/util_socket/socket_read_uint64.c
@@ -0,0 +1,28 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
+
+#include "../unix/unix_shadow.h"
+
+int nondet_status();
+uint64_t nondet_uint64();
+
+int socket_read_uint64(uint64_t* val, int s)
+{
+    /* verify that this is an open fd. */
+    MODEL_ASSERT(prop_is_open_fd(s));
+
+    /* prove access by writing a nondet value to val. */
+    /* also, this covers partial read scenarios from read(). */
+    *val = nondet_uint64();
+
+    /* does this read succeed? */
+    if (0 == nondet_success())
+    {
+        return STATUS_SUCCESS;
+    }
+    else
+    {
+        return ERROR_SOCKET_READ;
+    }
+}
