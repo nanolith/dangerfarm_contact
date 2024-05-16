df8bf41e5b38f355592c28e9801fe05b427ba3da
Justin Handville <nanolith@gmail.com>
2024-05-15 22:41:17 -0400

    Model checks for socket_read_contact_form_data.

---
+++ models/socket_read_contact_form_data_main.c
@@ -0,0 +1,57 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/data/contact_form.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+size_t nondet_size();
+
+/* simulate different data sizes. */
+size_t data_size()
+{
+    size_t size = nondet_size();
+    if (size > 10)
+    {
+        size = 10;
+    }
+
+    return size;
+}
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    uint64_t val = 0;
+    contact_form hdr;
+    char data[20];
+
+    /* initialize the unix shadow interface. */
+    unix_shadow_init();
+
+    /* create a socket. */
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    /* "read" contact form data from this socket. */
+    retval = socket_read_contact_form_data(data, data_size(), sock);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto close_sock;
+    }
+
+    /* success. */
+    retval = 0;
+    goto close_sock;
+
+close_sock:
+    close(sock);
+
+done:
+    return retval;
+}
