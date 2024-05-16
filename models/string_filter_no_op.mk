9b112ca2a397e034c98fd8f3e82e51a4b96cf7c7
Justin Handville <nanolith@gmail.com>
2024-05-15 22:37:25 -0400

    MCs for string_filter_no_op shadow methods.

---
+++ models/string_filter_no_op.mk
@@ -0,0 +1,11 @@
+ALL:
+	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
+	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
+	--drop-unused-functions \
+	--unwind 10 --unwinding-assertions \
+	-I ../include \
+	shadow/libcontact/string/string_create_nondet.c \
+	shadow/libcontact/string/string_release.c \
+	shadow/libcontact/string/string_filter_no_op.c \
+	string_filter_no_op_main.c
