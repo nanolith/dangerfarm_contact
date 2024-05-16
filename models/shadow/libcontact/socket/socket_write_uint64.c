bdffb52cd51e4c6bee5ec64cc4c1e1b531f4d3fd
Justin Handville <nanolith@gmail.com>
2024-05-15 22:39:14 -0400

    socket_write_uint64 shadow impl.

---
+++ models/shadow/libcontact/socket/socket_write_uint64.c
@@ -0,0 +1,23 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
+
+#include "../../unix/unix_shadow.h"
+
+int nondet_status();
+
+int socket_write_uint64(int s, uint64_t val)
+{
+    /* verify that this is an open fd. */
+    MODEL_ASSERT(prop_is_open_fd(s));
+
+    /* does this write succeed? */
+    if (0 == nondet_success())
+    {
+        return STATUS_SUCCESS;
+    }
+    else
+    {
+        return ERROR_SOCKET_WRITE;
+    }
+}
