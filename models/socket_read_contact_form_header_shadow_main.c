9d56f0048c727f85a6ee37b480462a23964aa062
Justin Handville <nanolith@gmail.com>
2024-05-18 21:54:53 -0400

    model checks for shadow method.

---
+++ models/socket_read_contact_form_header_shadow_main.c
@@ -0,0 +1,47 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/data/contact_form.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "shadow/unix/unix_shadow.h"
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    uint64_t val = 0;
+    contact_form hdr;
+
+    /* initialize the unix shadow interface. */
+    unix_shadow_init();
+
+    /* create a socket. */
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    /* "read" a contact form header from this socket, using the shadow read. */
+    retval = socket_read_contact_form_header(&hdr, sock);
+    if (STATUS_SUCCESS != retval)
+    {
+        /* if it fails, it fails due to a socket read error. */
+        MODEL_ASSERT(ERROR_SOCKET_READ == retval);
+        retval = 1;
+        goto close_sock;
+    }
+
+    /* success. */
+    retval = 0;
+    goto close_sock;
+
+close_sock:
+    close(sock);
+
+done:
+    return retval;
+}
