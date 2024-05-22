8146c4a58459cc6f286be077991b2f006fbe8558
Justin Handville <nanolith@gmail.com>
2024-05-21 21:30:46 -0400

    Added valid contact form prop assertion.

--- src/libcontact/contact_form_extract_subject.c
+++ src/libcontact/contact_form_extract_subject.c
@@ -1,3 +1,4 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
 #include <dangerfarm_contact/data/contact_form.h>
 #include <dangerfarm_contact/util/string.h>
 
@@ -17,6 +18,8 @@
  */
 int contact_form_extract_subject(char** subject, const contact_form* form)
 {
+    MODEL_ASSERT(prop_valid_contact_form(form));
+
     size_t offset = form->name_size + form->email_size;
 
     return string_create(subject, form->data + offset, form->subject_size);
