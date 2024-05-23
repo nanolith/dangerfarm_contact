4f8e0cb2ddedc17729abf2ecd30a697087c61f97
Justin Handville <nanolith@gmail.com>
2024-05-22 22:06:02 -0400

    Reduced loop count in analysis.

--- models/contact_form_extract_comment_nondet.mk
+++ models/contact_form_extract_comment_nondet.mk
@@ -4,6 +4,8 @@ ALL:
 	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
 	--drop-unused-functions \
 	--unwind 10 --unwinding-assertions \
+	--unwindset contact_form_create.0:4,contact_form_create.1:4 \
+	--unwindset contact_form_create.2:4,contact_form_create.3:4 \
 	-I ../include \
 	../src/libcontact/contact_form_compute_size.c \
 	../src/libcontact/contact_form_release.c \
