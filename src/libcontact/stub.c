87cb9ff4c1f364031495ea50e0a9d557c494e4e0
Justin Handville <nanolith@gmail.com>
2024-05-24 22:03:12 -0400

    Added the database protocol header to the stub.

--- src/libcontact/stub.c
+++ src/libcontact/stub.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <dangerfarm_contact/data/contact_form.h>
+#include <dangerfarm_contact/protocol/database.h>
 #include <dangerfarm_contact/status_codes.h>
 #include <dangerfarm_contact/util/socket.h>
 #include <dangerfarm_contact/util/string.h>
