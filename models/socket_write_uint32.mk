bc31a8be447eee82fed5d5c3069d1b1370db6f40
Justin Handville <nanolith@gmail.com>
2024-05-26 19:45:58 -0400

    socket_write_uint32 model checks.

---
+++ models/socket_write_uint32.mk
@@ -0,0 +1,14 @@
+ALL:
+	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
+	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
+	--drop-unused-functions \
+	--unwind 10 --unwinding-assertions \
+	-I ../include \
+	shadow/unix/close.c \
+	shadow/unix/prop_is_open_fd.c \
+	shadow/unix/write.c \
+	shadow/unix/socket.c \
+	shadow/unix/unix_shadow_init.c \
+	../src/libcontact/socket_write_uint32.c \
+	socket_write_uint32_main.c
---
+++ models/socket_write_uint32_main.c
@@ -0,0 +1,45 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "shadow/unix/unix_shadow.h"
+
+uint32_t nondet_uint32();
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    uint32_t val = nondet_uint32();
+
+    /* initialize the unix shadow interface. */
+    unix_shadow_init();
+
+    /* create a socket. */
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    /* "write" to this socket. */
+    retval = socket_write_uint32(sock, val);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto close_sock;
+    }
+
+    /* success. */
+    retval = 0;
+    goto close_sock;
+
+close_sock:
+    close(sock);
+
+done:
+    return retval;
+}
