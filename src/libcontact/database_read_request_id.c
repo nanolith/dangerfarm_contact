700ee9e750c4c1d2eeee4cf82de3ef913c84bcd9
Justin Handville <nanolith@gmail.com>
2024-05-26 19:46:45 -0400

    database_read_request_id impl.

---
+++ src/libcontact/database_read_request_id.c
@@ -0,0 +1,17 @@
+#include <dangerfarm_contact/protocol/database.h>
+#include <dangerfarm_contact/util/socket.h>
+
+/**
+ * \brief Read a database request id from the socket.
+ *
+ * \param req       The request to read.
+ * \param s         The socket from which this request is read.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int database_read_request_id(uint32_t* req, int s)
+{
+    return socket_read_uint32(req, s);
+}
