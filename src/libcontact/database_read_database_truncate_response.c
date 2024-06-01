e15d2f59eec8d70cec3327d7eb690afcc7021d8e
Justin Handville <nanolith@gmail.com>
2024-05-31 21:47:09 -0400

    database_read_database_truncate_response impl.

---
+++ src/libcontact/database_read_database_truncate_response.c
@@ -0,0 +1,42 @@
+#include <dangerfarm_contact/protocol/database.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
+
+/**
+ * \brief Read a database truncate response from the socket.
+ *
+ * \param status    The status for this response.
+ * \param s         The socket from which this request is read.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int database_read_database_truncate_response(uint32_t* status, int s)
+{
+    int retval;
+    uint32_t request_id;
+
+    /* read the request id. */
+    retval = socket_read_uint32(&request_id, s);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* verify the request id. */
+    if (DATABASE_REQUEST_ID_TRUNCATE != request_id)
+    {
+        return ERROR_DATABASE_PROTOCOL_UNEXPECTED_REQUEST_ID;
+    }
+
+    /* read the status. */
+    retval = socket_read_uint32(status, s);
+    if (STATUS_SUCCESS != retval)
+    {
+        return retval;
+    }
+
+    /* success. */
+    return STATUS_SUCCESS;
+}
