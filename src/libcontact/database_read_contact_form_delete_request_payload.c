7996846b95ac24494354a0547673562362f03721
Justin Handville <nanolith@gmail.com>
2024-05-31 21:38:19 -0400

    database_read_contact_form_delete_request_payload impl.

---
+++ src/libcontact/database_read_contact_form_delete_request_payload.c
@@ -0,0 +1,18 @@
+#include <dangerfarm_contact/protocol/database.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
+
+/**
+ * \brief Read a contact delete request payload from the socket.
+ *
+ * \param id        Pointer to receive the contact form id on success.
+ * \param s         The socket from which this request is read.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int database_read_contact_form_delete_request_payload(uint64_t* id, int s)
+{
+    return socket_read_uint64(id, s);
+}
