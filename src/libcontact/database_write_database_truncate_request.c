d92e02f9453d924ea19c92dea8d694082daa3d1b
Justin Handville <nanolith@gmail.com>
2024-05-31 21:43:39 -0400

    database_write_database_truncate_request impl.

---
+++ src/libcontact/database_write_database_truncate_request.c
@@ -0,0 +1,18 @@
+#include <dangerfarm_contact/protocol/database.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
+
+/**
+ * \brief Write a database truncate request to the socket.
+ *
+ * \param s         The socket to which this request is written.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int database_write_database_truncate_request(int s)
+{
+    /* write the request id. */
+    return socket_write_uint32(s, DATABASE_REQUEST_ID_TRUNCATE);
+}
