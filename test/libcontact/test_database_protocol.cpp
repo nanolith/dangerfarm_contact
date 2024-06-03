460acfa01457f3cd5bce7dbb52c54a5dc2d078ae
Justin Handville <nanolith@gmail.com>
2024-06-02 22:28:24 -0400

    db r/w contact form delete response unit tests.

--- test/libcontact/test_database_protocol.cpp
+++ test/libcontact/test_database_protocol.cpp
@@ -514,3 +514,35 @@ TEST(database_read_write_contact_form_delete_request)
     TEST_ASSERT(0 == close(sock[0]));
     TEST_ASSERT(0 == close(sock[1]));
 }
+
+/**
+ * We can read and write a contact form delete response.
+ */
+TEST(database_read_write_contact_form_delete_response)
+{
+    int sock[2];
+    const uint32_t WRITE_STATUS = 2233;
+    uint32_t status = 9999;
+
+    /* we can create a socket pair. */
+    TEST_ASSERT(0 == socketpair(AF_UNIX, SOCK_STREAM, 0, sock));
+
+    /* write the delete response. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == database_write_contact_form_delete_response(
+                    sock[0], WRITE_STATUS));
+
+    /* read the response payload. */
+    TEST_ASSERT(
+        STATUS_SUCCESS
+            == database_read_contact_form_delete_response(
+                    &status, sock[1]));
+
+    /* the statuses match. */
+    TEST_EXPECT(WRITE_STATUS == status);
+
+    /* clean up. */
+    TEST_ASSERT(0 == close(sock[0]));
+    TEST_ASSERT(0 == close(sock[1]));
+}
