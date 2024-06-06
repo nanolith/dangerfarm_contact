66e2715d90e125a64993179e6a8e095076c1b769
Justin Handville <nanolith@gmail.com>
2024-06-05 21:11:15 -0400

    database_write_contact_form_get_count_request model checks.

---
+++ models/database_write_contact_form_get_count_request.mk
@@ -0,0 +1,15 @@
+ALL:
+	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
+	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
+	--drop-unused-functions \
+	--unwind 10 --unwinding-assertions \
+	-I ../include \
+	shadow/libcontact/socket/socket_write_uint32.c \
+	shadow/unix/close.c \
+	shadow/unix/prop_is_open_fd.c \
+	shadow/unix/write.c \
+	shadow/unix/socket.c \
+	shadow/unix/unix_shadow_init.c \
+	../src/libcontact/database_write_contact_form_get_count_request.c \
+	database_write_contact_form_get_count_request_main.c
---
+++ models/database_write_contact_form_get_count_request_main.c
@@ -0,0 +1,45 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/protocol/database.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "shadow/unix/unix_shadow.h"
+
+uint32_t nondet_uint32();
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    uint32_t val = nondet_uint32();
+
+    /* initialize the unix shadow interface. */
+    unix_shadow_init();
+
+    /* create a socket. */
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    /* "write" to this socket. */
+    retval = database_write_contact_form_get_count_request(sock);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto close_sock;
+    }
+
+    /* success. */
+    retval = 0;
+    goto close_sock;
+
+close_sock:
+    close(sock);
+
+done:
+    return retval;
+}
