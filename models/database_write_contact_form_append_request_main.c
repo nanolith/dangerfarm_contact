c2f30323fef3087f238a1358966ac2940f13894d
Justin Handville <nanolith@gmail.com>
2024-06-05 21:06:43 -0400

    Added missing header to model check.

--- models/database_write_contact_form_append_request_main.c
+++ models/database_write_contact_form_append_request_main.c
@@ -1,5 +1,6 @@
 #include <dangerfarm_contact/cbmc/model_assert.h>
 #include <dangerfarm_contact/data/contact_form.h>
+#include <dangerfarm_contact/protocol/database.h>
 #include <dangerfarm_contact/status_codes.h>
 #include <sys/socket.h>
 #include <sys/types.h>
