bbb5360fe2f722dbb26507c9e3f20298e7d0d5d7
Justin Handville <nanolith@gmail.com>
2024-06-06 23:15:09 -0400

    database_read_contact_form_read_request_payload model checks.

---
+++ models/database_read_contact_form_read_request_payload_main.c
@@ -0,0 +1,43 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <dangerfarm_contact/util/socket.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "shadow/unix/unix_shadow.h"
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    uint64_t id = 0;
+
+    /* initialize the unix shadow interface. */
+    unix_shadow_init();
+
+    /* create a socket. */
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    /* "read" the read request payload from this socket. */
+    retval = database_read_contact_form_read_request_payload(&id, sock);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto close_sock;
+    }
+
+    /* success. */
+    retval = 0;
+    goto close_sock;
+
+close_sock:
+    close(sock);
+
+done:
+    return retval;
+}
