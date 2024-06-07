8ae8f6d3ba85fd8552241b20da73de53d875a247
Justin Handville <nanolith@gmail.com>
2024-06-06 23:12:27 -0400

    Added constrained loop shadow read impl.

---
+++ models/shadow/unix/read_loop_constrained.c
@@ -0,0 +1,55 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <errno.h>
+#include <stddef.h>
+#include <unistd.h>
+
+#include "unix_shadow.h"
+
+int nondet_status();
+size_t nondet_readbytes();
+char nondet_char();
+
+ssize_t
+read(int fd, void* buf, size_t nbytes)
+{
+    char* cbuf = (char*)buf;
+
+    /* verify that this is an open fd. */
+    MODEL_ASSERT(prop_is_open_fd(fd));
+
+    /* does this read succeed? */
+    if (0 == nondet_status())
+    {
+        /* compute the size read. */
+        size_t readbytes = nondet_readbytes();
+        if (readbytes > nbytes)
+        {
+            readbytes = nbytes;
+        }
+
+        /* set a byte at the end of the read range to prove access. */
+        if (readbytes > 0)
+        {
+            cbuf[readbytes - 1] = nondet_char();
+        }
+
+        /* truncate readbytes. */
+        if (readbytes > 8)
+        {
+            readbytes = 8;
+        }
+
+        /* throw in some randomized bytes. */
+        for (size_t i = 0; i < readbytes; ++i)
+        {
+            cbuf[i] = nondet_char();
+        }
+
+        return (ssize_t)readbytes;
+    }
+    else
+    {
+        errno = EBADF;
+        return -1;
+    }
+}
