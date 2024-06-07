877403a271d71af92c7fb6a7cdf0a7b5a3706647
Justin Handville <nanolith@gmail.com>
2024-06-06 23:17:54 -0400

    database_write_contact_form_delete_request model checks.

---
+++ models/database_write_contact_form_delete_request_main.c
@@ -0,0 +1,46 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/protocol/database.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "shadow/unix/unix_shadow.h"
+
+uint64_t nondet_uint64();
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    uint64_t id = nondet_uint64();
+
+    /* initialize the unix shadow interface. */
+    unix_shadow_init();
+
+    /* create a socket. */
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    /* "write" a contact form delete request to this socket. */
+    retval = database_write_contact_form_delete_request(sock, id);
+    if (STATUS_SUCCESS != retval)
+    {
+        MODEL_ASSERT(ERROR_SOCKET_WRITE == retval);
+        retval = 1;
+        goto close_sock;
+    }
+
+    /* success. */
+    retval = 0;
+    goto close_sock;
+
+close_sock:
+    close(sock);
+
+done:
+    return retval;
+}
