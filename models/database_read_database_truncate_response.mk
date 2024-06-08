fb4a43338e49fc505a0159aa7faab7d269f74eb5
Justin Handville <nanolith@gmail.com>
2024-06-07 21:46:24 -0400

    database_read_database_truncate_response model checks.

---
+++ models/database_read_database_truncate_response.mk
@@ -0,0 +1,19 @@
+ALL:
+	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
+	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
+	--drop-unused-functions \
+	--unwind 10 --unwinding-assertions \
+	--unwindset contact_form_create.0:4,contact_form_create.1:4 \
+	--unwindset contact_form_create.2:4,contact_form_create.3:4 \
+	-I ../include \
+	shadow/unix/close.c \
+	shadow/unix/prop_is_open_fd.c \
+	shadow/unix/socket.c \
+	shadow/unix/unix_shadow_init.c \
+	shadow/libcontact/socket/socket_read_uint32.c \
+	shadow/libcontact/string/string_filter_no_op.c \
+	shadow/libcontact/string/string_release.c \
+	shadow/libcontact/string/prop_string_valid.c \
+	../src/libcontact/database_read_database_truncate_response.c \
+	database_read_database_truncate_response_main.c
