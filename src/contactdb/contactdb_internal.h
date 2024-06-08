6e0747f9f4bef1f8286af1f90411b91a57a0766a
Justin Handville <nanolith@gmail.com>
2024-06-08 18:04:29 -0400

    Added root_capabilities to contactdb_context.

--- src/contactdb/contactdb_internal.h
+++ src/contactdb/contactdb_internal.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <stdint.h>
+
 typedef struct contactdb_context contactdb_context;
 struct contactdb_context
 {
     char* db_path;
     char* socket_path;
+    uint64_t root_capabilities;
 };
