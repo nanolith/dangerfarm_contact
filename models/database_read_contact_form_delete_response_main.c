26d8c2d2beceaf66c7227924b158fbfc0d8e69d3
Justin Handville <nanolith@gmail.com>
2024-06-07 21:42:25 -0400

    database_read_contact_form_delete_response model checks.

---
+++ models/database_read_contact_form_delete_response_main.c
@@ -0,0 +1,43 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/protocol/database.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "shadow/unix/unix_shadow.h"
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    uint32_t status;
+
+    /* initialize the unix shadow interface. */
+    unix_shadow_init();
+
+    /* create a socket. */
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    /* "read" database contact form delete request payload from the socket. */
+    retval = database_read_contact_form_delete_response(&status, sock);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto close_sock;
+    }
+
+    /* success. */
+    retval = 0;
+    goto close_sock;
+
+close_sock:
+    close(sock);
+
+done:
+    return retval;
+}
