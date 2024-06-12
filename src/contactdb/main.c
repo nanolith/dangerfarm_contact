ad6a56f66f9780052ff3b2571baeb938b488a4dd
Justin Handville <nanolith@gmail.com>
2024-06-12 19:30:55 -0400

    Create contactdb context on startup.

--- src/contactdb/main.c
+++ src/contactdb/main.c
@@ -1,3 +1,4 @@
+#include <dangerfarm_contact/status_codes.h>
 #include <stdio.h>
 
 #include "contactdb_connection.h"
@@ -5,9 +6,28 @@
 
 int main(int argc, char* argv[])
 {
-    (void)argc;
-    (void)argv;
+    contactdb_context* ctx;
+    int retval, release_retval;
 
-    printf("Not yet implemented.\n");
-    return 1;
+    /* create the contactdb context. */
+    retval = contactdb_context_create_from_arguments(&ctx, argc, argv);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    /* success. */
+    retval = STATUS_SUCCESS;
+    goto cleanup_ctx;
+
+cleanup_ctx:
+    release_retval = contactdb_context_release(ctx);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = 2;
+    }
+
+done:
+    return retval;
 }
