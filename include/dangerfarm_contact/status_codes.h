c7f51d10f49346d2cdc9a615933b8f9dda0c3758
Justin Handville <nanolith@gmail.com>
2024-06-15 15:51:05 -0400

    contactdb fork error decl.

--- include/dangerfarm_contact/status_codes.h
+++ include/dangerfarm_contact/status_codes.h
@@ -34,3 +34,4 @@
 #define ERROR_CONTACTDB_ACCEPT                               31
 #define ERROR_CONTACTDB_PERMISSION_DENIED                    32
 #define ERROR_CONTACTDB_SIGACTION_FAILURE                    33
+#define ERROR_CONTACTDB_FORK                                 34
