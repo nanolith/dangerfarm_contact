9b4e0bb3f588c0de0a18b1b465e60f5b8e565dde
Justin Handville <nanolith@gmail.com>
2024-06-16 01:15:46 -0400

    database_write_generic_response model checks.

---
+++ models/database_write_generic_response_main.c
@@ -0,0 +1,46 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/data/contact_form.h>
+#include <dangerfarm_contact/protocol/database.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "shadow/unix/unix_shadow.h"
+
+uint32_t nondet_u32();
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    uint32_t status = nondet_u32();
+
+    /* initialize the unix shadow interface. */
+    unix_shadow_init();
+
+    /* create a socket. */
+    int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sock < 0)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    /* "write" the generic response. */
+    retval = database_write_generic_response(sock, nondet_u32(), status);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto close_sock;
+    }
+
+    /* success. */
+    retval = 0;
+    goto close_sock;
+
+close_sock:
+    close(sock);
+
+done:
+    return retval;
+}
