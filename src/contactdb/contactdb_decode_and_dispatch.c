c31b2a8e18a6bc25bac47be0b587642b8fd20f7d
Justin Handville <nanolith@gmail.com>
2024-06-16 01:13:47 -0400

    contactdb_decode_and_dispatch stub impl.

---
+++ src/contactdb/contactdb_decode_and_dispatch.c
@@ -0,0 +1,41 @@
+#include <dangerfarm_contact/protocol/database.h>
+#include <dangerfarm_contact/status_codes.h>
+
+#include "contactdb_internal.h"
+
+/**
+ * \brief Decode and dispatch a contactdb request.
+ *
+ * \param ctx           The context for this operation.
+ * \param sock          The socket for this operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on error.
+ */
+int contactdb_decode_and_dispatch(contactdb_context* ctx, int sock)
+{
+    (void)ctx;
+    int retval;
+    uint32_t req;
+
+    /* read the request id. */
+    retval = database_read_request_id(&req, sock);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto done;
+    }
+
+    /* decode the request id. */
+    switch (req)
+    {
+        default:
+            retval =
+                database_write_generic_response(
+                    sock, req, ERROR_DATABASE_PROTOCOL_UNEXPECTED_REQUEST_ID);
+            break;
+    }
+
+done:
+    return retval;
+}
