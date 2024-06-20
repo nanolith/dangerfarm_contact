e4f44cf4a47746e460d5184026ec4a1068459cc0
Justin Handville <nanolith@gmail.com>
2024-06-19 22:52:29 -0400

    contactdb_dnd_contact_form_get decl.

--- src/contactdb/contactdb_internal.h
+++ src/contactdb/contactdb_internal.h
@@ -151,3 +151,15 @@ int contactdb_dnd_contact_form_get_count(contactdb_context* ctx, int sock);
  *      - non-zero on error.
  */
 int contactdb_dnd_contact_form_get_list(contactdb_context* ctx, int sock);
+
+/**
+ * \brief Decode and dispatch a contactdb contact form get request.
+ *
+ * \param ctx           The context for this operation.
+ * \param sock          The socket for this operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on error.
+ */
+int contactdb_dnd_contact_form_get(contactdb_context* ctx, int sock);
