55aad0b5104ce4a9599c3ff4ec309091f3814c2b
Justin Handville <nanolith@gmail.com>
2024-06-19 22:56:14 -0400

    contactdb_dnd_contact_form_delete decl.

--- src/contactdb/contactdb_internal.h
+++ src/contactdb/contactdb_internal.h
@@ -163,3 +163,15 @@ int contactdb_dnd_contact_form_get_list(contactdb_context* ctx, int sock);
  *      - non-zero on error.
  */
 int contactdb_dnd_contact_form_get(contactdb_context* ctx, int sock);
+
+/**
+ * \brief Decode and dispatch a contactdb contact form delete request.
+ *
+ * \param ctx           The context for this operation.
+ * \param sock          The socket for this operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on error.
+ */
+int contactdb_dnd_contact_form_delete(contactdb_context* ctx, int sock);
