7cf24d7023fdc86f12a48448f4ca8106ff723770
Justin Handville <nanolith@gmail.com>
2024-06-20 22:40:29 -0400

    ctool stub.

---
+++ src/ctool/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * \brief Main entry point for ctool.
+ *
+ * \param argc          The argument count.
+ * \param argv          The argument vector.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int main(int argc, char* argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    printf("Not yet implemented.\n");
+
+    return 1;
+}
