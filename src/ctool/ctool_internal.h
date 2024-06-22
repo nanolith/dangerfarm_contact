c44153dcf4d4b13835684bb008ddf175c80a290a
Justin Handville <nanolith@gmail.com>
2024-06-21 22:00:59 -0400

    form_id ctool_context member decl.

--- src/ctool/ctool_internal.h
+++ src/ctool/ctool_internal.h
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <stdint.h>
+
 typedef struct ctool_context ctool_context;
 struct ctool_context
 {
     char* db_path;
     char* socket_path;
     int sock;
+    uint64_t form_id;
 };
