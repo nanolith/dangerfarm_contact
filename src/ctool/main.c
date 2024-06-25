48d8d5c1071cd82913805138d8fce710a11db76b
Justin Handville <nanolith@gmail.com>
2024-06-24 21:34:26 -0400

    Create context in ctool main.

--- src/ctool/main.c
+++ src/ctool/main.c
@@ -1,3 +1,4 @@
+#include <dangerfarm_contact/status_codes.h>
 #include <stdio.h>
 
 #include "ctool_internal.h"
@@ -14,10 +15,28 @@
  */
 int main(int argc, char* argv[])
 {
-    (void)argc;
-    (void)argv;
+    ctool_context* ctx;
+    int retval, release_retval;
+
+    /* create the ctool context. */
+    retval = ctool_context_create_from_arguments(&ctx, argc, argv);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto done;
+    }
 
     printf("Not yet implemented.\n");
+    retval = 1;
+    goto cleanup_ctx;
 
-    return 1;
+cleanup_ctx:
+    release_retval = ctool_context_release(ctx);
+    if (STATUS_SUCCESS != release_retval)
+    {
+        retval = 2;
+    }
+
+done:
+    return retval;
 }
