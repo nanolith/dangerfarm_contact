7ad7f1972eff0014315d4667280d1457c6710350
Justin Handville <nanolith@gmail.com>
2024-07-02 23:12:44 -0400

    Fixed capitalization.

--- src/libcontact/status_decode.c
+++ src/libcontact/status_decode.c
@@ -57,7 +57,7 @@ const char* status_decode(int status)
         case ERROR_DATABASE_DBI_OPEN:
             return "database open handle.";
         case ERROR_DATABASE_TXN_COMMIT:
-            return "Database commit transaction.";
+            return "database commit transaction.";
         case ERROR_DATABASE_PUT:
             return "Database put.";
         case ERROR_DATABASE_GET:
