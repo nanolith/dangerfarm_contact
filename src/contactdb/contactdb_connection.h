a652f4275500ed69070f7dcef2abe74011adf8d7
Justin Handville <nanolith@gmail.com>
2024-07-11 21:30:21 -0400

    contactdb_connection_form_get_first decl.

--- src/contactdb/contactdb_connection.h
+++ src/contactdb/contactdb_connection.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <lmdb.h>
+#include <stdbool.h>
 
 /* 20 MB is the default contact database size. */
 #define CONTACTDB_SIZE      (20 * 1024 * 1024)
@@ -118,3 +119,23 @@ int contactdb_connection_counter_get(
  */
 int contactdb_connection_form_append(
     contactdb_connection* conn, MDB_txn* txn, const contact_form* form);
+
+/**
+ * \brief Get the first key associated with the contact database.
+ *
+ * \param cursor        Pointer to receive the cursor for this operation on
+ *                      success.
+ * \param conn          The connection for this operation.
+ * \param txn           The transaction for this operation.
+ * \param key           The key for this operation.
+ * \param val           The value for this operation.
+ * \param found         Pointer to the boolean value to be set to true if a
+ *                      record was found.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int contactdb_connection_form_get_first(
+    MDB_cursor** cursor, contactdb_connection* conn, MDB_txn* txn, MDB_val* key,
+    MDB_val* val, bool* found);
