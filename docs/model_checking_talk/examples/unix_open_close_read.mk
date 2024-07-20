bb32264eb2882d7cbc533354394babd2fd50818a
Justin Handville <nanolith@gmail.com>
2024-07-19 22:50:53 -0400

    Initial unix open / close example model check.

---
+++ docs/model_checking_talk/examples/unix_open_close_read.mk
@@ -0,0 +1,11 @@
+ALL:
+	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
+	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
+	--drop-unused-functions \
+	--unwind 10 --unwinding-assertions \
+	-I ../include \
+	shadow/open.c \
+	shadow/close.c \
+	shadow/prop_is_valid_unix_desc.c \
+	open_close_read_example_main.c
