e4c1d8cd7db945e74f06e61e4c79fdf52d688737
Justin Handville <nanolith@gmail.com>
2024-07-20 21:16:00 -0400

    heap corruption example.

---
+++ docs/model_checking_talk/examples/heap_corruption.mk
@@ -0,0 +1,12 @@
+ALL:
+	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
+	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
+	--drop-unused-functions \
+	--unwind 10 --unwinding-assertions \
+	-I ../include \
+	shadow/close.c \
+	shadow/open.c \
+	shadow/prop_is_valid_unix_desc.c \
+	shadow/read.c \
+	heap_corruption_main.c
