334e89685c79d57b9344d309a4ccc68fb6afc1ed
Justin Handville <nanolith@gmail.com>
2024-07-20 21:20:38 -0400

    Added resource leak example.

---
+++ docs/model_checking_talk/examples/resource_leak.mk
@@ -0,0 +1,13 @@
+ALL:
+	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
+	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
+	--drop-unused-functions \
+	--unwind 10 --unwinding-assertions \
+	-I ../include \
+	shadow/close.c \
+	shadow/open.c \
+	shadow/prop_is_valid_unix_desc.c \
+	shadow/read.c \
+	shadow/write.c \
+	resource_leak_main.c
