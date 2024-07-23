9bd362f91d0dce9961ba1792a537b68b5ad9831e
Justin Handville <nanolith@gmail.com>
2024-07-22 20:52:42 -0400

    Add match_count and refactor test in terms of it.

--- test/libcontact/test_status_decode.cpp
+++ test/libcontact/test_status_decode.cpp
@@ -4,16 +4,34 @@
 
 TEST_SUITE(status_decode);
 
+static size_t match_count(const char* str, const char** arr, size_t size)
+{
+    size_t count = 0;
+
+    for (size_t i = 0; i < size; ++i)
+    {
+        if (str == arr[i])
+            ++count;
+    }
+
+    return count;
+}
+
 /**
  * Test that each status is decoded into unique strings by class.
  */
 TEST(decode_strings)
 {
+    const char* unique_strings[1024];
+    size_t unique_ctr = 0;
+
     /* verify that an unknown status is decoded as an unknown string. */
     const char* unknown_string = status_decode(-1);
     TEST_ASSERT(0 == strcmp(unknown_string, "unknown error"));
+    unique_strings[unique_ctr++] = unknown_string;
 
     /* verify the success string. */
     const char* success_string = status_decode(STATUS_SUCCESS);
-    TEST_EXPECT(success_string != unknown_string);
+    TEST_EXPECT(0 == match_count(success_string, unique_strings, unique_ctr));
+    unique_strings[unique_ctr++] = success_string;
 }
