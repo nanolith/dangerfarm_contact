abec912467df2093d94e8d81eb5a4e08d3ff1750
Justin Handville <nanolith@gmail.com>
2024-07-22 20:47:13 -0400

    contactform_context_release impl.

---
+++ src/contactform/contactform_context_release.c
@@ -0,0 +1,52 @@
+#include <dangerfarm_contact/status_codes.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+#include "contactform_internal.h"
+
+/**
+ * \brief Release a \ref contactform_context instance.
+ *
+ * \param ctx           The context to release.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int contactform_context_release(contactform_context* ctx)
+{
+    int retval = STATUS_SUCCESS, release_retval;
+    int dummy_retval;
+
+    /* close the socket if open. */
+    if (ctx->dbsock >= 0)
+    {
+        release_retval = close(ctx->dbsock);
+        if (release_retval < 0)
+        {
+            retval = ERROR_CONTACTFORM_DBSOCK_CLOSE;
+        }
+    }
+
+    /* kill the child process if spawned. */
+    if (ctx->dbpid > 1)
+    {
+        /* kill the child process. */
+        release_retval = kill(ctx->dbpid, SIGTERM);
+        if (release_retval < 0)
+        {
+            retval = ERROR_CONTACTFORM_DBPID_KILL;
+        }
+        else
+        {
+            /* wait on the child process. */
+            release_retval = waitpid(ctx->dbpid, &dummy_retval, 0);
+            if (release_retval < 0)
+            {
+                retval = ERROR_CONTACTFORM_DBPID_WAIT;
+            }
+        }
+    }
+
+    return retval;
+}
