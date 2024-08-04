0b2b72bf21d00fd222d0d8f4e18cd71079370d7f
Justin Handville <nanolith@gmail.com>
2024-08-03 23:42:59 -0400

    prop_is_valid_contactdb_connection decl.

--- src/contactdb/contactdb_connection.h
+++ src/contactdb/contactdb_connection.h
@@ -188,3 +188,12 @@ int contactdb_connection_form_get_first(
 int contactdb_connection_form_get_next(
     MDB_cursor* cursor, MDB_val* key, MDB_val* val, bool* found,
     uint64_t* p_key);
+
+/**
+ * \brief Return true if the given \ref contactdb_connection is valid.
+ *
+ * \param conn          The connection to verify.
+ *
+ * \returns true if this connection is valid, and false otherwise.
+ */
+bool prop_is_valid_contactdb_connection(const contactdb_connection* conn);
