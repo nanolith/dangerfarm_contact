05623f34db387f7a4a5d067831a6b8f7da8762a9
Justin Handville <nanolith@gmail.com>
2024-08-08 22:28:20 -0400

    mdb_txn_commit shadow model checks.

---
+++ models/mdb_txn_begin_commit_shadow_main.c
@@ -0,0 +1,72 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <dangerfarm_contact/status_codes.h>
+#include <lmdb.h>
+#include <stddef.h>
+
+#include "shadow/lmdb/lmdb_internal.h"
+
+int main(int argc, char* argv[])
+{
+    int retval;
+    MDB_env* env;
+    MDB_txn* txn;
+
+    /* Create the MDB environment. */
+    retval = mdb_env_create(&env);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto done;
+    }
+
+    int flags = 0;
+    #ifdef __OpenBSD__
+    flags |= MDB_WRITEMAP;
+    #endif
+
+    /* attempt to open the database. */
+    retval = mdb_env_open(env, "path/to/db", flags, 0600);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto cleanup_env;
+    }
+
+    /* we are not currently in a transaction. */
+    MODEL_ASSERT(!prop_MDB_env_in_txn(env));
+
+    /* begin a transaction. */
+    retval = mdb_txn_begin(env, NULL, 0, &txn);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto cleanup_env;
+    }
+
+    /* we are in a transaction. */
+    MODEL_ASSERT(prop_MDB_env_in_txn(env));
+
+    /* commit the transaction. */
+    retval = mdb_txn_commit(txn);
+    if (STATUS_SUCCESS != retval)
+    {
+        retval = 1;
+        goto txn_abort;
+    }
+
+    /* we are no longer in a transaction. */
+    MODEL_ASSERT(!prop_MDB_env_in_txn(env));
+
+    /* success. */
+    retval = 0;
+    goto cleanup_env;
+
+txn_abort:
+    mdb_txn_abort(txn);
+
+cleanup_env:
+    mdb_env_close(env);
+
+done:
+    return retval;
+}
