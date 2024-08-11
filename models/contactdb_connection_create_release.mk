8adce9f1c0dbe2af5dc3c05f0a9631872336c923
Justin Handville <nanolith@gmail.com>
2024-08-10 22:16:36 -0400

    Use lmdb shadow include.

--- models/contactdb_connection_create_release.mk
+++ models/contactdb_connection_create_release.mk
@@ -1,3 +1,5 @@
+include shadow/lmdb/lmdb.mk
+
 ALL:
 	cbmc --bounds-check --pointer-check --memory-leak-check \
 	--malloc-may-fail --malloc-fail-null \
@@ -5,22 +7,7 @@ ALL:
 	--drop-unused-functions \
 	--unwind 10 --unwinding-assertions \
 	--unwindset string_create.0:7 \
-	-I ../include -I $(HOME)/apps/lmdb/include \
-	shadow/lmdb/mdb_dbi_close.c \
-	shadow/lmdb/mdb_dbi_open.c \
-	shadow/lmdb/mdb_env_close.c \
-	shadow/lmdb/mdb_env_create.c \
-	shadow/lmdb/mdb_env_open.c \
-	shadow/lmdb/mdb_env_sync.c \
-	shadow/lmdb/mdb_env_set_mapsize.c \
-	shadow/lmdb/mdb_env_set_maxdbs.c \
-	shadow/lmdb/mdb_txn_abort.c \
-	shadow/lmdb/mdb_txn_begin.c \
-	shadow/lmdb/mdb_txn_commit.c \
-	shadow/lmdb/prop_MDB_env_created.c \
-	shadow/lmdb/prop_MDB_env_in_txn.c \
-	shadow/lmdb/prop_MDB_env_opened.c \
-	shadow/lmdb/prop_MDB_txn_valid.c \
+	-I ../include -I $(HOME)/apps/lmdb/include $(LMDB_SHADOW_FILES) \
 	../src/contactdb/contactdb_connection_create.c \
 	../src/contactdb/contactdb_connection_release.c \
 	contactdb_connection_create_release_main.c
