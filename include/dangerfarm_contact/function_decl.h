0656e6c40c13599fbba46a24f651d9e0aa937250
Justin Handville <nanolith@gmail.com>
2024-08-12 22:24:29 -0400

    DANGERFARM_CONTACT_CONCAT decl.

--- include/dangerfarm_contact/function_decl.h
+++ include/dangerfarm_contact/function_decl.h
@@ -4,3 +4,8 @@
  * A UUID symbol to make names unique.
  */
 #define DANGERFARM_CONTACT_UNIQUE_NAME 6a32746c_5353_11ef_b2d0_18c04d8c76eb
+
+/**
+ * Concatenate two symbols together.
+ */
+#define DANGERFARM_CONTACT_CONCAT(x,y) x ## y
