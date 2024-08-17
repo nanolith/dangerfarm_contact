a5f1402b7007b9d43adc234b0835192b7de192c6
Justin Handville <nanolith@gmail.com>
2024-08-17 18:34:27 -0400

    Fixed database_read_contact_form_append_response export.

--- include/dangerfarm_contact/protocol/database.h
+++ include/dangerfarm_contact/protocol/database.h
@@ -363,7 +363,7 @@ int DANGERFARM_CONTACT_SYM(database_write_generic_response)(
             return  DANGERFARM_CONTACT_SYM( \
                 database_write_contact_form_append_response)(x,y); \
     } \
-    static inline int database_read_contact_form_append_response( \
+    static inline int sym ## database_read_contact_form_append_response( \
         uint32_t* x, int y) { \
             return DANGERFARM_CONTACT_SYM( \
                         database_read_contact_form_append_response)(x,y); \
