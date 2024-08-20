a052fde4ca0bef2ce8c622e4dc2fd01fb5cf4c2f
Justin Handville <nanolith@gmail.com>
2024-08-19 22:48:26 -0400

    socket_read_uint32 export symbol.

--- src/libcontact/database_read_contact_form_read_response.c
+++ src/libcontact/database_read_contact_form_read_response.c
@@ -4,6 +4,7 @@
 #include <dangerfarm_contact/util/socket.h>
 
 DANGERFARM_CONTACT_IMPORT_contact_form;
+DANGERFARM_CONTACT_IMPORT_util_socket;
 
 /**
  * \brief Read a contact read response from the socket.
