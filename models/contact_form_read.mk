3501a3a5116039e86bf683e70caabec6cd8ef053
Justin Handville <nanolith@gmail.com>
2024-08-21 21:31:44 -0400

    Fixed unwindset.

--- models/contact_form_read.mk
+++ models/contact_form_read.mk
@@ -1,11 +1,13 @@
+DF_EXPORT=dangerfarm_contact_6a32746c_5353_11ef_b2d0_18c04d8c76eb_V0_1_
+
 ALL:
 	cbmc --bounds-check --pointer-check --memory-leak-check \
 	--malloc-may-fail --malloc-fail-null \
 	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
 	--drop-unused-functions \
 	--unwind 10 --unwinding-assertions \
-	--unwindset contact_form_create.0:4,contact_form_create.1:4 \
-	--unwindset contact_form_create.2:4,contact_form_create.3:4 \
+	--unwindset contact_form_create_nondet.0:4,contact_form_create_nondet.1:4 \
+	--unwindset contact_form_create_nondet.2:4,contact_form_create_nondet.3:4 \
 	-I ../include \
 	shadow/unix/close.c \
 	shadow/unix/prop_is_open_fd.c \
