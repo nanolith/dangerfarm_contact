52a6821eef1ed45276cfd6659b48e3c6d7b92afc
Justin Handville <nanolith@gmail.com>
2024-08-21 21:25:56 -0400

    Fixed unwindset.

--- models/contact_form_extract_email.mk
+++ models/contact_form_extract_email.mk
@@ -1,12 +1,14 @@
+DF_EXPORT=dangerfarm_contact_6a32746c_5353_11ef_b2d0_18c04d8c76eb_V0_1_
+
 ALL:
 	cbmc --bounds-check --pointer-check --memory-leak-check \
 	--malloc-may-fail --malloc-fail-null \
 	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
 	--drop-unused-functions \
 	--unwind 10 --unwinding-assertions \
-	--unwindset contact_form_create.0:4,contact_form_create.1:4 \
-	--unwindset contact_form_create.2:4,contact_form_create.3:4 \
-	--unwindset string_create.0:7 \
+	--unwindset contact_form_create_nondet.0:4,contact_form_create_nondet.1:4 \
+	--unwindset contact_form_create_nondet.2:4,contact_form_create_nondet.3:4 \
+	--unwindset $(DF_EXPORT)string_create.0:7 \
 	-I ../include \
 	shadow/libcontact/data/prop_valid_contact_form.c \
 	shadow/libcontact/data/contact_form_create_nondet.c \
