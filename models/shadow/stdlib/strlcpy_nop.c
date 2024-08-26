df93a9ca068e171627d13c5d2d84b666f0bc27e0
Justin Handville <nanolith@gmail.com>
2024-08-25 22:16:12 -0400

    nop strlcpy shadow impl.

---
+++ models/shadow/stdlib/strlcpy_nop.c
@@ -0,0 +1,18 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <stddef.h>
+
+size_t nondet_size();
+
+size_t strlcpy(char * restrict dst, const char * restrict src, size_t dstsize)
+{
+    MODEL_ASSERT(NULL != dst);
+    MODEL_ASSERT(NULL != src);
+
+    size_t retsize = nondet_size();
+    if (retsize > dstsize)
+    {
+        retsize = dstsize;
+    }
+
+    return retsize;
+}
