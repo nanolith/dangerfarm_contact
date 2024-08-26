e9080d08ab0fb519da366e5a35577687c2b59537
Justin Handville <nanolith@gmail.com>
2024-08-25 22:24:06 -0400

    chmod shadow impl.

---
+++ models/shadow/unix/chmod.c
@@ -0,0 +1,39 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+#include "unix_shadow.h"
+
+int nondet_retval();
+
+int chmod(const char *path, mode_t mode)
+{
+    MODEL_ASSERT(NULL != path);
+    MODEL_ASSERT((S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP) == mode);
+
+    int retval = nondet_retval();
+
+    switch (retval)
+    {
+        case 0:
+            return 0;
+
+        case EACCES:
+        case EFAULT:
+        case EFTYPE:
+        case EINTEGRITY:
+        case EIO:
+        case ELOOP:
+        case ENAMETOOLONG:
+        case ENOENT:
+        case ENOTDIR:
+        case EPERM:
+        case EROFS:
+            errno = retval;
+            return -1;
+
+        default:
+            errno = EACCES;
+            return -1;
+    }
+}
