55c0d82bb260108d17685c4b70972332234982b6
Justin Handville <nanolith@gmail.com>
2024-08-25 22:17:25 -0400

    nop shadow unlink impl.

---
+++ models/shadow/unix/unlink_nop.c
@@ -0,0 +1,32 @@
+#include <dangerfarm_contact/cbmc/model_assert.h>
+#include <errno.h>
+#include <unistd.h>
+
+int nondet_retval();
+
+int unlink(const char *path)
+{
+    MODEL_ASSERT(NULL != path);
+
+    int retval = nondet_retval();
+    switch (retval)
+    {
+        case 0:
+            return 0;
+
+        case ENOTDIR:
+        case EISDIR:
+        case ENAMETOOLONG:
+        case ENOENT:
+        case EACCES:
+        case ELOOP:
+        case EPERM:
+        case EIO:
+        case EINTEGRITY:
+        case EROFS:
+        case EFAULT:
+        case ENOSPC:
+            errno = retval;
+            return -1;
+    }
+}
