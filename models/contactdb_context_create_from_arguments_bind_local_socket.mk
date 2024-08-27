2dec39c853ada2cf8981e721604e6683e62bf5f8
Justin Handville <nanolith@gmail.com>
2024-08-26 22:08:57 -0400

    build for bind local socket model checks.

---
+++ models/contactdb_context_create_from_arguments_bind_local_socket.mk
@@ -0,0 +1,23 @@
+ALL:
+	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
+	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
+	--drop-unused-functions \
+	--unwind 10 --unwinding-assertions \
+	-I ../include -I $(HOME)/apps/lmdb/include \
+	shadow/libcontact/string/string_release.c \
+	shadow/stdlib/strdup.c \
+	shadow/stdlib/strcmp.c \
+	shadow/stdlib/strlcpy_nop.c \
+	shadow/unix/socket.c \
+	shadow/unix/unlink_nop.c \
+	shadow/unix/bind_nop.c \
+	shadow/unix/chmod.c \
+	shadow/unix/listen.c \
+	shadow/unix/prop_is_open_fd.c \
+	shadow/unix/close.c \
+	shadow/contactdb/contactdb_context_create_from_arguments_set_string.c \
+	shadow/contactdb/contactdb_connection_release_nop.c \
+	../src/contactdb/contactdb_context_create_from_arguments_bind_local_socket.c \
+	../src/contactdb/contactdb_context_release.c \
+	contactdb_context_create_from_arguments_bind_local_socket_main.c
