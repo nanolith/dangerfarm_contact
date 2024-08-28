3cde3df9cff05fa6c5c1348447d9c1a891984721
Justin Handville <nanolith@gmail.com>
2024-08-27 22:23:27 -0400

    contactdb_disable_signal_handler decl.

--- src/contactdb/contactdb_internal.h
+++ src/contactdb/contactdb_internal.h
@@ -83,6 +83,17 @@ bool contactdb_has_capability(const contactdb_context* ctx, const uint64_t cap);
 int contactdb_daemonize(contactdb_context* ctx);
 
 /**
+ * \brief Disable the signal handler.
+ *
+ * \param ctx           The context to use for this operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+int contactdb_disable_signal_handler(contactdb_context* ctx);
+
+/**
  * \brief Drop privileges to only those needed for database communication.
  *
  * \param ctx           The context for this operation.
