4327985ae6370959e27567a35e0992900df7a9fc
Justin Handville <nanolith@gmail.com>
2024-08-27 22:26:10 -0400

    contactdb_disable_signal_handler shadow impl.

---
+++ models/shadow/contactdb/contactdb_disable_signal_handler.c
@@ -0,0 +1,8 @@
+#include <dangerfarm_contact/status_codes.h>
+
+#include "../../../src/contactdb/contactdb_internal.h"
+
+int contactdb_disable_signal_handler(contactdb_context* ctx)
+{
+    return STATUS_SUCCESS;
+}
