53d2110bd8a232966e42b8a7e10601b9e56b6b89
Justin Handville <nanolith@gmail.com>
2024-08-31 22:16:41 -0400

    Use socketed context.

--- models/contactdb_has_capability.mk
+++ models/contactdb_has_capability.mk
@@ -7,7 +7,7 @@ ALL:
 	-I ../include -I $(HOME)/apps/lmdb/include \
 	shadow/contactdb/contactdb_connection_create.c \
 	shadow/contactdb/contactdb_connection_release_nop.c \
-	shadow/contactdb/contactdb_context_create_from_arguments_no_socket.c \
+	shadow/contactdb/contactdb_context_create_from_arguments.c \
 	shadow/contactdb/contactdb_disable_signal_handler.c \
 	shadow/libcontact/string/string_release.c \
 	shadow/unix/close.c \
