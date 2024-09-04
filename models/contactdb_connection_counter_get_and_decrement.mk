9f605fc3aee5e560e0de7cea214b033649156809
Justin Handville <nanolith@gmail.com>
2024-09-03 21:19:15 -0400

    contactdb_connection_counter_get_and_decrement MC build.

---
+++ models/contactdb_connection_counter_get_and_decrement.mk
@@ -0,0 +1,13 @@
+include shadow/lmdb/lmdb.mk
+
+ALL:
+	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
+	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
+	--drop-unused-functions \
+	--unwind 10 --unwinding-assertions \
+	-I ../include -I $(HOME)/apps/lmdb/include $(LMDB_SHADOW_FILES) \
+	../src/contactdb/contactdb_connection_counter_get_and_decrement.c \
+	../src/contactdb/contactdb_connection_create.c \
+	../src/contactdb/contactdb_connection_release.c \
+	contactdb_connection_counter_get_and_decrement_main.c
