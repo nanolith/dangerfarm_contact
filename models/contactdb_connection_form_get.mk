70d76899cb9f97d00b3e09225911643b79d8e50e
Justin Handville <nanolith@gmail.com>
2024-09-05 21:17:22 -0400

    contactdb_connection_form_get MC build.

---
+++ models/contactdb_connection_form_get.mk
@@ -0,0 +1,14 @@
+include shadow/lmdb/lmdb.mk
+
+ALL:
+	cbmc --bounds-check --pointer-check --memory-leak-check \
+	--malloc-may-fail --malloc-fail-null \
+	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
+	--drop-unused-functions \
+	--unwind 10 --unwinding-assertions \
+	--unwind 10 \
+	-I ../include -I $(HOME)/apps/lmdb/include $(LMDB_SHADOW_FILES) \
+	../src/contactdb/contactdb_connection_create.c \
+	../src/contactdb/contactdb_connection_release.c \
+	../src/contactdb/contactdb_connection_form_get.c \
+	contactdb_connection_form_get_main.c
