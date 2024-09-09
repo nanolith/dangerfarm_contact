d11044eead129b2f7ac4a1534b90ac2222bc7fc7
Justin Handville <nanolith@gmail.com>
2024-09-08 22:52:41 -0400

    Added lmdb unwindset.

--- models/contactdb_accept_and_dispatch.mk
+++ models/contactdb_accept_and_dispatch.mk
@@ -6,6 +6,7 @@ ALL:
 	--div-by-zero-check --pointer-overflow-check --trace --stop-on-fail -DCBMC \
 	--drop-unused-functions \
 	--unwind 10 --unwinding-assertions \
+	$(LMDB_SHADOW_UNWINDSET) \
 	-I ../include -I $(HOME)/apps/lmdb/include $(LMDB_SHADOW_FILES) \
 	shadow/contactdb/contactdb_context_create_from_arguments.c \
 	shadow/contactdb/contactdb_connection_create.c \
