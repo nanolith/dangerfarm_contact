89ecb04b7e253630a864e59328b2e3ea1c5bdd15
Justin Handville <nanolith@gmail.com>
2024-09-13 23:18:07 -0400

    MODEL_CONTRACT_PRECONDITION_BEGIN decl.

--- include/dangerfarm_contact/cbmc/function_contracts.h
+++ include/dangerfarm_contact/cbmc/function_contracts.h
@@ -1,5 +1,8 @@
 #pragma once
 
 #ifdef CBMC
+#define MODEL_CONTRACT_PRECONDITION_BEGIN(function, ...) \
+    inline bool function_precondition_begin(function, #__VA_ARGS__) {
 #else
+#define MODEL_CONTRACT_PRECONDITION_BEGIN(function)
 #endif
