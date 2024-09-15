3b590cbce12607819be250990242edc6055fa9ef
Justin Handville <nanolith@gmail.com>
2024-09-14 22:50:23 -0400

    Added missing line continuation to macro.

--- include/dangerfarm_contact/cbmc/function_contracts.h
+++ include/dangerfarm_contact/cbmc/function_contracts.h
@@ -5,7 +5,7 @@
     inline bool function_precondition_begin(function, #__VA_ARGS__) {
 #define MODEL_CONTRACT_PRECONDITION_END(function) \
     }
-#define MODEL_CONTRACT_POSTCONDITION_BEGIN(function, ...)
+#define MODEL_CONTRACT_POSTCONDITION_BEGIN(function, ...) \
     inline bool function_precondition_begin(function, #__VA_ARGS__) {
 #define MODEL_CONTRACT_POSTCONDITION_END(function) \
     }
