1d30051ba79dc501a302fec212a1b5560da6b7af
Justin Handville <nanolith@gmail.com>
2024-09-16 22:01:03 -0400

    Added MODEL_CHECK_OBJECT_READ macro.

--- include/dangerfarm_contact/cbmc/model_assert.h
+++ include/dangerfarm_contact/cbmc/model_assert.h
@@ -5,10 +5,14 @@
 
 #ifdef CBMC
 #define MODEL_ASSERT(x) __CPROVER_assert((x), #x); REQUIRE_SEMICOLON_HERE
+#define MODEL_CHECK_OBJECT_READ(x, size) \
+    __CPROVER_ASSERT(__CPROVER_r_ok((x), (size)), #x "read " #size); \
+    REQUIRE_SEMICOLON_HERE
 #define MODEL_ASSUME(x) __CPROVER_assume((x)); REQUIRE_SEMICOLON_HERE
 #define MODEL_EXEMPT(x)
 #else
 #define MODEL_ASSERT(x) REQUIRE_SEMICOLON_HERE
+#define MODEL_CHECK_OBJECT_READ(x, size) REQUIRE_SEMICOLON_HERE
 #define MODEL_ASSUME(x) REQUIRE_SEMICOLON_HERE
 #define MODEL_EXEMPT(x) x
 #endif
