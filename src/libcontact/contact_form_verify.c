36b56f34e63c1dbbf93d5f588f92c7bcf3d003e2
Justin Handville <nanolith@gmail.com>
2024-09-17 22:09:53 -0400

    Added function contract to contact_form_verify.

--- src/libcontact/contact_form_verify.c
+++ src/libcontact/contact_form_verify.c
@@ -17,19 +17,33 @@ DANGERFARM_CONTACT_IMPORT_contact_form;
 int DANGERFARM_CONTACT_SYM(contact_form_verify)(
     const DANGERFARM_CONTACT_SYM(contact_form)* form, size_t size)
 {
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        DANGERFARM_CONTACT_SYM(contact_form_verify), form, size);
+
+    int retval;
+
     /* the size must at least be large enough for the contact form. */
     if (size < sizeof(contact_form))
     {
-        return ERROR_CONTACT_FORM_INVALID;
+        retval = ERROR_CONTACT_FORM_INVALID;
+        goto done;
     }
 
     /* the computed size should match the given size. */
     size_t computed_size = contact_form_compute_size(form);
     if (computed_size != size)
     {
-        return ERROR_CONTACT_FORM_INVALID;
+        retval = ERROR_CONTACT_FORM_INVALID;
+        goto done;
     }
 
     /* the contact form size is valid. */
-    return STATUS_SUCCESS;
+    retval = STATUS_SUCCESS;
+    goto done;
+
+done:
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        DANGERFARM_CONTACT_SYM(contact_form_verify), retval);
+
+    return retval;
 }
