cdb113ca42105543af35deec842507200b1d3f06
Justin Handville <nanolith@gmail.com>
2024-09-19 22:13:12 -0400

    contact_form_extract_subject added function contracts.

--- src/libcontact/contact_form_extract_subject.c
+++ src/libcontact/contact_form_extract_subject.c
@@ -22,9 +22,16 @@ DANGERFARM_CONTACT_IMPORT_util_string;
 int DANGERFARM_CONTACT_SYM(contact_form_extract_subject)(
     char** subject, const DANGERFARM_CONTACT_SYM(contact_form)* form)
 {
-    MODEL_ASSERT(prop_valid_contact_form(form));
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        DANGERFARM_CONTACT_SYM(contact_form_extract_subject), subject, form);
 
+    int retval;
     size_t offset = form->name_size + form->email_size;
 
-    return string_create(subject, form->data + offset, form->subject_size);
+    retval = string_create(subject, form->data + offset, form->subject_size);
+
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        DANGERFARM_CONTACT_SYM(contact_form_extract_subject), retval, subject);
+
+    return retval;
 }
