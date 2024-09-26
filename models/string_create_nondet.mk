387f2ee76076ecb1d0837cbc90eef9524022eb99
Justin Handville <nanolith@gmail.com>
2024-09-25 22:35:14 -0400

    Added missing string prop source file.

--- models/string_create_nondet.mk
+++ models/string_create_nondet.mk
@@ -5,6 +5,7 @@ ALL:
 	--drop-unused-functions \
 	--unwind 10 --unwinding-assertions \
 	-I ../include \
+	shadow/libcontact/string/prop_string_valid.c \
 	shadow/libcontact/string/string_create_nondet.c \
 	shadow/libcontact/string/string_release.c \
 	string_create_nondet_main.c
