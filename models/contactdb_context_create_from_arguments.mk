a43e7a22b47136eeb923e1b3b083517ae6fc17bd
Justin Handville <nanolith@gmail.com>
2024-09-27 22:35:11 -0400

    Added missing source file.

--- models/contactdb_context_create_from_arguments.mk
+++ models/contactdb_context_create_from_arguments.mk
@@ -10,6 +10,7 @@ ALL:
 	shadow/contactdb/contactdb_connection_create.c \
 	shadow/contactdb/contactdb_connection_release_nop.c \
 	shadow/contactdb/contactdb_disable_signal_handler.c \
+	shadow/libcontact/string/prop_string_valid.c \
 	shadow/libcontact/string/string_release.c \
 	../src/contactdb/contactdb_context_create_from_arguments.c \
 	../src/contactdb/contactdb_context_release.c \
