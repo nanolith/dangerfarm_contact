e648b681e21fd10a9a0172472ce4f94ec75610b9
Justin Handville <nanolith@gmail.com>
2024-09-30 22:18:40 -0400

    Removed redundant assertion.

--- models/shadow/libcontact/socket/socket_read_uint32.c
+++ models/shadow/libcontact/socket/socket_read_uint32.c
@@ -14,9 +14,6 @@ int DANGERFARM_CONTACT_SYM(socket_read_uint32)(uint32_t* val, int s)
 
     int retval;
 
-    /* verify that this is an open fd. */
-    MODEL_ASSERT(prop_is_open_fd(s));
-
     /* prove access by writing a nondet value to val. */
     /* also, this covers partial read scenarios from read(). */
     *val = nondet_uint32();
