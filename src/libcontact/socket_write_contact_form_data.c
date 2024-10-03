b44b72412b0b6241e0d43564c74078e343d69501
Justin Handville <nanolith@gmail.com>
2024-10-02 22:35:11 -0400

    socket_write_contact_form_data enforce function contracts.

--- src/libcontact/socket_write_contact_form_data.c
+++ src/libcontact/socket_write_contact_form_data.c
@@ -16,14 +16,27 @@
 int DANGERFARM_CONTACT_SYM(socket_write_contact_form_data)(
     int s, const char* data, size_t size)
 {
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        DANGERFARM_CONTACT_SYM(socket_write_contact_form_data), s, data, size);
+
+    int retval;
     ssize_t wrote_bytes = 0;
 
     /* write the data. */
     wrote_bytes = write(s, data, size);
     if (wrote_bytes < 0 || (size_t)wrote_bytes != size)
     {
-        return ERROR_SOCKET_WRITE;
+        retval = ERROR_SOCKET_WRITE;
+        goto done;
     }
 
-    return STATUS_SUCCESS;
+    /* success. */
+    retval = STATUS_SUCCESS;
+    goto done;
+
+done:
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        DANGERFARM_CONTACT_SYM(socket_write_contact_form_data), retval);
+
+    return retval;
 }
