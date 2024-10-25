6863c4b8fc6c36ddeb7e95f7661fde39b37b9b42
Justin Handville <nanolith@gmail.com>
2024-10-24 22:32:23 -0400

    Add return code check enforcement.

--- include/dangerfarm_contact/protocol/database.h
+++ include/dangerfarm_contact/protocol/database.h
@@ -77,7 +77,8 @@ DANGERFARM_CONTACT_SYM(database_read_contact_form_append_request_payload)(
  *      - zero on success.
  *      - non-zero on failure.
  */
-int DANGERFARM_CONTACT_SYM(database_write_contact_form_append_response)(
+int FN_DECL_MUST_CHECK
+DANGERFARM_CONTACT_SYM(database_write_contact_form_append_response)(
     int s, const uint32_t status);
 
 /**
