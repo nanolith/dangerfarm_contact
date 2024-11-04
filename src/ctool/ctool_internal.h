87765b889b94a2676354d2d59da3fa2e2d0be11f
Justin Handville <nanolith@gmail.com>
2024-11-03 22:05:56 -0500

    Add return code check enforcement.

--- src/ctool/ctool_internal.h
+++ src/ctool/ctool_internal.h
@@ -77,7 +77,8 @@ ctool_drop_privileges(ctool_context* ctx);
  *      - zero on success.
  *      - non-zero on failure.
  */
-int ctool_run_command(ctool_context* ctx);
+int FN_DECL_MUST_CHECK
+ctool_run_command(ctool_context* ctx);
 
 /**
  * \brief Run an append command.
