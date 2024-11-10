5a7ea9b6e23081fe5d20b81f046a9f504f0595c6
Justin Handville <nanolith@gmail.com>
2024-11-10 13:08:33 -0500

    database_write_contact_form_list_request function contract postconditions.

--- include/dangerfarm_contact/protocol/database.h
+++ include/dangerfarm_contact/protocol/database.h
@@ -341,6 +341,13 @@ MODEL_CONTRACT_PRECONDITIONS_BEGIN(
 MODEL_CONTRACT_PRECONDITIONS_END(
     DANGERFARM_CONTACT_SYM(database_write_contact_form_list_request))
 
+/* postconditions. */
+MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
+    DANGERFARM_CONTACT_SYM(database_write_contact_form_list_request),
+    int retval)
+MODEL_CONTRACT_POSTCONDITIONS_END(
+    DANGERFARM_CONTACT_SYM(database_write_contact_form_list_request))
+
 /**
  * \brief Write a list contact forms response to the socket.
  *
