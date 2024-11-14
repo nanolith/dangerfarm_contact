df82782b328e7c05e12b3e1123ef9a6f6e095372
Justin Handville <nanolith@gmail.com>
2024-11-13 22:36:44 -0500

    database_write_contact_form_read_request function contract postconditions.

--- include/dangerfarm_contact/protocol/database.h
+++ include/dangerfarm_contact/protocol/database.h
@@ -478,6 +478,13 @@ MODEL_CONTRACT_PRECONDITIONS_BEGIN(
 MODEL_CONTRACT_PRECONDITIONS_END(
     DANGERFARM_CONTACT_SYM(database_write_contact_form_read_request))
 
+/* postconditions. */
+MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
+    DANGERFARM_CONTACT_SYM(database_write_contact_form_read_request),
+    int retval)
+MODEL_CONTRACT_POSTCONDITIONS_END(
+    DANGERFARM_CONTACT_SYM(database_write_contact_form_read_request))
+
 /**
  * \brief Read a contact read request payload from the socket.
  *
