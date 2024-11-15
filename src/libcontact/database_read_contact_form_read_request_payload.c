8b068a3deb5717e2abf4b7cf755b24174e3a0c66
Justin Handville <nanolith@gmail.com>
2024-11-14 23:03:37 -0500

    Added function contract enforcement.

--- src/libcontact/database_read_contact_form_read_request_payload.c
+++ src/libcontact/database_read_contact_form_read_request_payload.c
@@ -17,5 +17,33 @@ DANGERFARM_CONTACT_IMPORT_util_socket;
 int DANGERFARM_CONTACT_SYM(database_read_contact_form_read_request_payload)(
     uint64_t* id, int s)
 {
-    return socket_read_uint64(id, s);
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        DANGERFARM_CONTACT_SYM(database_read_contact_form_read_request_payload),
+        id, s);
+
+    /* read the id. */
+    int retval = socket_read_uint64(id, s);
+    if (STATUS_SUCCESS != retval)
+    {
+        *id = DATABASE_PROTOCOL_INVALID_ID;
+        goto done;
+    }
+
+    /* if the id is invalid, that's an error. */
+    if (DATABASE_PROTOCOL_INVALID_ID == *id)
+    {
+        retval = ERROR_DATABASE_PROTOCOL_INVALID_ID;
+        goto done;
+    }
+
+    /* success. */
+    retval = STATUS_SUCCESS;
+    goto done;
+
+done:
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        DANGERFARM_CONTACT_SYM(database_read_contact_form_read_request_payload),
+        retval, id);
+
+    return retval;
 }
