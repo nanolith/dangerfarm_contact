44a2814110cd850512f163f83e8a3ccf2d35fc07
Justin Handville <nanolith@gmail.com>
2024-11-15 23:13:47 -0500

    Added function contract enforcement.

--- src/libcontact/database_write_contact_form_read_response.c
+++ src/libcontact/database_write_contact_form_read_response.c
@@ -21,35 +21,48 @@ int DANGERFARM_CONTACT_SYM(database_write_contact_form_read_response)(
     int s, const uint32_t status,
     const DANGERFARM_CONTACT_SYM(contact_form)* form)
 {
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        DANGERFARM_CONTACT_SYM(database_write_contact_form_read_response),
+        s, status, form);
+
     int retval;
 
     /* write the request id. */
     retval = socket_write_uint32(s, DATABASE_REQUEST_ID_CONTACT_FORM_GET);
     if (STATUS_SUCCESS != retval)
     {
-        return retval;
+        goto done;
     }
 
     /* write the status. */
     retval = socket_write_uint32(s, status);
     if (STATUS_SUCCESS != retval)
     {
-        return retval;
+        goto done;
     }
 
     /* skip the contact form payload if the status was not successful. */
     if (STATUS_SUCCESS != status)
     {
-        return STATUS_SUCCESS;
+        retval = STATUS_SUCCESS;
+        goto done;
     }
 
     /* write the contact form data. */
     retval = contact_form_write(s, form);
     if (STATUS_SUCCESS != retval)
     {
-        return retval;
+        goto done;
     }
 
     /* success. */
-    return STATUS_SUCCESS;
+    retval = STATUS_SUCCESS;
+    goto done;
+
+done:
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        DANGERFARM_CONTACT_SYM(database_write_contact_form_read_response),
+        retval);
+
+    return retval;
 }
