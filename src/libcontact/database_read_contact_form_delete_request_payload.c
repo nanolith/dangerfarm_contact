e31452b0b82b9ef0e97bfc3aa6888a891221eb15
Justin Handville <nanolith@gmail.com>
2024-11-17 22:15:42 -0500

    Added function contract enforcement.

--- src/libcontact/database_read_contact_form_delete_request_payload.c
+++ src/libcontact/database_read_contact_form_delete_request_payload.c
@@ -17,5 +17,35 @@ DANGERFARM_CONTACT_IMPORT_util_socket;
 int DANGERFARM_CONTACT_SYM(database_read_contact_form_delete_request_payload)(
     uint64_t* id, int s)
 {
-    return socket_read_uint64(id, s);
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        DANGERFARM_CONTACT_SYM(
+            database_read_contact_form_delete_request_payload), id, s);
+
+    /* read the id. */
+    int retval = socket_read_uint64(id, s);
+    if (STATUS_SUCCESS != retval)
+    {
+        goto fail;
+    }
+
+    /* if the id is invalid, that's an error. */
+    if (DATABASE_PROTOCOL_INVALID_ID == *id)
+    {
+        retval = ERROR_DATABASE_PROTOCOL_INVALID_ID;
+        goto fail;
+    }
+
+    /* success. */
+    retval = STATUS_SUCCESS;
+    goto done;
+
+fail:
+    *id = DATABASE_PROTOCOL_INVALID_ID;
+
+done:
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        DANGERFARM_CONTACT_SYM(
+            database_read_contact_form_delete_request_payload), retval, id);
+
+    return retval;
 }
