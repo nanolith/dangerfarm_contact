7c43d77124f22ae5580428102915f47e1582039f
Justin Handville <nanolith@gmail.com>
2024-11-20 18:54:07 -0500

    database_write_database_truncate_response function contract postconditions.

--- include/dangerfarm_contact/protocol/database.h
+++ include/dangerfarm_contact/protocol/database.h
@@ -823,6 +823,13 @@ MODEL_CONTRACT_PRECONDITIONS_BEGIN(
 MODEL_CONTRACT_PRECONDITIONS_END(
     DANGERFARM_CONTACT_SYM(database_write_database_truncate_response))
 
+/* postconditions. */
+MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
+    DANGERFARM_CONTACT_SYM(database_write_database_truncate_response),
+    int retval)
+MODEL_CONTRACT_POSTCONDITIONS_END(
+    DANGERFARM_CONTACT_SYM(database_write_database_truncate_response))
+
 /**
  * \brief Read a database truncate response from the socket.
  *
