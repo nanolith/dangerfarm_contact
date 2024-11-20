abaa28f61788977e5aceed95b6de853cfe09708a
Justin Handville <nanolith@gmail.com>
2024-11-19 23:11:32 -0500

    database_write_database_truncate_request function contract preconditions.

--- include/dangerfarm_contact/protocol/database.h
+++ include/dangerfarm_contact/protocol/database.h
@@ -785,6 +785,14 @@ MODEL_CONTRACT_POSTCONDITIONS_END(
 int FN_DECL_MUST_CHECK
 DANGERFARM_CONTACT_SYM(database_write_database_truncate_request)(int s);
 
+/* preconditions. */
+MODEL_CONTRACT_PRECONDITIONS_BEGIN(
+    DANGERFARM_CONTACT_SYM(database_write_database_truncate_request), int s)
+        /* socket is a valid descriptor. */
+        MODEL_ASSERT(prop_is_open_fd(s));
+MODEL_CONTRACT_PRECONDITIONS_END(
+    DANGERFARM_CONTACT_SYM(database_write_database_truncate_request))
+
 /**
  * \brief Write a database truncate response to the socket.
  *
