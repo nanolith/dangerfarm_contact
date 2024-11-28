988a5e0234d5dde38103fd6fc676d8ffcfa96c38
Justin Handville <nanolith@gmail.com>
2024-11-27 20:06:21 -0500

    contactdb_drop_privileges shadow function contract enforcement.

--- models/shadow/contactdb/contactdb_drop_privileges.c
+++ models/shadow/contactdb/contactdb_drop_privileges.c
@@ -6,17 +6,24 @@ int nondet_retval();
 
 int contactdb_drop_privileges(contactdb_context* ctx)
 {
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(contactdb_drop_privileges, ctx);
+
     int retval = nondet_retval();
 
     switch (retval)
     {
         case STATUS_SUCCESS:
-            return STATUS_SUCCESS;
+            retval = STATUS_SUCCESS;
+            goto done;
 
         case ERROR_CONTACTDB_DROP_PRIVILEGES:
-            return ERROR_CONTACTDB_DROP_PRIVILEGES;
-
         default:
-            return ERROR_CONTACTDB_DROP_PRIVILEGES;
+            retval = ERROR_CONTACTDB_DROP_PRIVILEGES;
+            goto done;
     }
+
+done:
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(contactdb_drop_privileges, retval);
+
+    return retval;
 }
