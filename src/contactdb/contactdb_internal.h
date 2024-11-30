c99359f0420ec995b47ea9f23006bc9bc6af4a85
Justin Handville <nanolith@gmail.com>
2024-11-29 22:40:33 -0500

    contactdb_dnd_contact_form_get_list function contract preconditions.

--- src/contactdb/contactdb_internal.h
+++ src/contactdb/contactdb_internal.h
@@ -357,6 +357,15 @@ MODEL_CONTRACT_POSTCONDITIONS_END(contactdb_dnd_contact_form_get_count)
 int FN_DECL_MUST_CHECK
 contactdb_dnd_contact_form_get_list(contactdb_context* ctx, int sock);
 
+/* preconditions. */
+MODEL_CONTRACT_PRECONDITIONS_BEGIN(
+    contactdb_dnd_contact_form_get_list, contactdb_context* ctx, int sock)
+        /* the context is valid. */
+        MODEL_ASSERT(prop_is_valid_contactdb_context(ctx));
+        /* the socket is valid. */
+        MODEL_ASSERT(prop_is_open_fd(sock));
+MODEL_CONTRACT_PRECONDITIONS_END(contactdb_dnd_contact_form_get_list)
+
 /**
  * \brief Decode and dispatch a contactdb contact form get request.
  *
