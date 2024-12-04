6a75654ee721b58840aa5ca66d342382fde6d6db
Justin Handville <nanolith@gmail.com>
2024-12-03 21:10:04 -0500

    contactdb_connection_counter_get_and_increment function contract enforcement.

--- models/shadow/contactdb/contactdb_connection_counter_get_and_increment.c
+++ models/shadow/contactdb/contactdb_connection_counter_get_and_increment.c
@@ -9,27 +9,43 @@ int nondet_retval();
 
 uint64_t nondet_value();
 
+uint64_t get_value()
+{
+    uint64_t retval = nondet_value();
+    MODEL_ASSUME(COUNTER_VALUE_INVALID != retval);
+
+    return retval;
+}
+
 int contactdb_connection_counter_get_and_increment(
     contactdb_connection* conn, MDB_txn* txn, uint64_t counter_id,
     uint64_t* value)
 {
-    MODEL_ASSERT(prop_is_valid_contactdb_connection(conn));
-    MODEL_ASSERT(prop_MDB_txn_valid(txn));
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        contactdb_connection_counter_get_and_increment,
+        conn, txn, counter_id, value);
+
     MODEL_ASSERT(prop_MDB_dbi_valid(conn->env, conn->global_db));
-    MODEL_ASSERT(
-        (counter_id == COUNTER_ID_CONTACT_COUNT)
-     || (counter_id == COUNTER_ID_CONTACT_KEY));
-    MODEL_ASSERT(NULL != value);
 
     int retval = nondet_retval();
     switch (retval)
     {
         case STATUS_SUCCESS:
-            *value = nondet_value();
-            return STATUS_SUCCESS;
+            *value = get_value();
+            goto done;
 
         case ERROR_DATABASE_GET:
         case ERROR_CONTACTDB_GET_INVALID_SIZE:
-            return retval;
+        case ERROR_CONTACTDB_BAD_COUNTER_VALUE:
+            goto fail;
     }
+
+fail:
+    *value = COUNTER_VALUE_INVALID;
+
+done:
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        contactdb_connection_counter_get_and_increment, retval, value);
+
+    return retval;
 }
