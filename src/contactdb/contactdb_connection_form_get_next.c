f8aa829dd7e92934375b42b2c5d7f4291813c78a
Justin Handville <nanolith@gmail.com>
2024-12-07 21:17:36 -0500

    Added function contract enforcement.

--- src/contactdb/contactdb_connection_form_get_next.c
+++ src/contactdb/contactdb_connection_form_get_next.c
@@ -21,33 +21,58 @@ int contactdb_connection_form_get_next(
     MDB_cursor* cursor, MDB_val* key, MDB_val* val, bool* found,
     uint64_t* p_key)
 {
+    MODEL_CONTRACT_CHECK_PRECONDITIONS(
+        contactdb_connection_form_get_next, cursor, key, val, found, p_key);
+
     int retval;
 
     /* get the next contact. */
     retval = mdb_cursor_get(cursor, key, val, MDB_NEXT);
     if (MDB_NOTFOUND == retval)
     {
-        *found = false;
-        return STATUS_SUCCESS;
+        retval = STATUS_SUCCESS;
+        goto fail;
     }
     else if (STATUS_SUCCESS != retval)
     {
-        return ERROR_DATABASE_CURSOR_GET;
+        retval = ERROR_DATABASE_CURSOR_GET;
+        goto fail;
     }
 
     /* verify the key size is correct. */
     if (key->mv_size != sizeof(uint64_t))
     {
-        return ERROR_CONTACTDB_GET_INVALID_SIZE;
+        retval = ERROR_CONTACTDB_GET_INVALID_SIZE;
+        goto fail;
     }
 
     /* copy the key. */
     if (NULL != p_key)
     {
         memcpy(p_key, key->mv_data, sizeof(uint64_t));
+
+        if (COUNTER_VALUE_INVALID == *p_key)
+        {
+            retval = ERROR_CONTACTDB_BAD_COUNTER_VALUE;
+            goto fail;
+        }
     }
 
     /* success. */
     *found = true;
-    return STATUS_SUCCESS;
+    retval = STATUS_SUCCESS;
+    goto done;
+
+fail:
+    *found = false;
+    *p_key = COUNTER_VALUE_INVALID;
+    key->mv_data = NULL; key->mv_size = 0;
+    val->mv_data = NULL; val->mv_size = 0;
+
+done:
+    MODEL_CONTRACT_CHECK_POSTCONDITIONS(
+        contactdb_connection_form_get_next, retval, cursor, key, val, found,
+        p_key);
+
+    return retval;
 }
