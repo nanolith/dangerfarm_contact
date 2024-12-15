656d9281b0fd15600ac7f90fb6dce2fee324ad66
Justin Handville <nanolith@gmail.com>
2024-12-14 22:14:06 -0500

    Handle DROP_PRIV_STEP_LIMIT_FS_ACCESS step.

--- src/contactform/contactform_drop_privileges.c
+++ src/contactform/contactform_drop_privileges.c
@@ -8,6 +8,7 @@
 #include "contactform_internal.h"
 
 /* forward decls. */
+static int reduce_filesystem_access(void);
 static int drop_privileges_for_parse(void);
 static int drop_all_privileges(void);
 
@@ -24,6 +25,9 @@ int contactform_drop_privileges(int step)
 {
     switch (step)
     {
+        case DROP_PRIV_STEP_LIMIT_FS_ACCESS:
+            return reduce_filesystem_access();
+
         case DROP_PRIV_STEP_KCGI_PARSE:
             return drop_privileges_for_parse();
 
@@ -33,6 +37,38 @@ int contactform_drop_privileges(int step)
         default:
             return ERROR_CONTACTFORM_INVALID_DROP_PRIVILEGES_STEP;
     }
+}
+
+/**
+ * \brief Reduce filesystem access prior to initialization.
+ *
+ * \returns a status code indicating success or failure.
+ *      - zero on success.
+ *      - non-zero on failure.
+ */
+static int reduce_filesystem_access(void)
+{
+    int retval;
+
+    (void)retval;
+
+    #ifdef __OpenBSD__
+    /* this process only accesses the contactdb application socket. */
+    retval = unveil(CONTACTDB_SOCKET, "rw");
+    if (STATUS_SUCCESS != retval)
+    {
+        return ERROR_CONTACTFORM_DROP_PRIVILEGES;
+    }
+
+    /* don't allow any other filesystem access. */
+    retval = unveil(NULL, NULL);
+    if (STATUS_SUCCESS != retval)
+    {
+        return ERROR_CONTACTFORM_DROP_PRIVILEGES;
+    }
+    #endif
+
+    return STATUS_SUCCESS;
 }
 
 /**
