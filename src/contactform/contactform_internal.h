fb1983f3ecb70dd2727db275abd093c971afdb14
Justin Handville <nanolith@gmail.com>
2024-12-18 22:41:40 -0500

    contactform_context_child_cleanup fix postcondition arguments.

--- src/contactform/contactform_internal.h
+++ src/contactform/contactform_internal.h
@@ -244,7 +244,7 @@ MODEL_CONTRACT_PRECONDITIONS_END(contactform_context_read_cgi)
 
 /* postconditions. */
 MODEL_CONTRACT_POSTCONDITIONS_BEGIN(
-    contactform_context_child_cleanup, int retval)
+    contactform_context_child_cleanup, contactform_context* ctx)
 MODEL_CONTRACT_POSTCONDITIONS_END(contactform_context_child_cleanup)
 
 /**
