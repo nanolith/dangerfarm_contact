53d4d8419f2beb13ee26f6435b0f29da192472e1
Justin Handville <nanolith@gmail.com>
2024-12-22 22:24:10 -0500

    fork / waitpid with pid tracking model checks.

---
+++ models/unix/fork_waitpid_with_pid_tracking/main.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+    pid_t pid;
+    int retval;
+
+    /* fork this process. */
+    pid = fork();
+    /* child */
+    if (0 == pid)
+    {
+        retval = 0;
+        goto done;
+    }
+    else if (pid > 0)
+    {
+        /* wait on the pid. */
+        int status = 0;
+        pid_t rpid = waitpid(pid, &status, 0);
+        if (rpid < 0)
+        {
+            retval = 1;
+            goto done;
+        }
+
+        MODEL_ASSERT(rpid == pid);
+
+        retval = 0;
+        goto done;
+    }
+    /* error case. */
+    else
+    {
+        retval = 1;
+        goto done;
+    }
+
+done:
+    return retval;
+}
